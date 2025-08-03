568f7465608e3e91ac89f5e36ae4c2f45ef3dc7e
Marcelo Beck <marcelobck@gmail.com>
2025-08-03 00:12:47 -0300

    DPK01 add comment

--- src/main/java/org/example/DPK01_impl_1.java
+++ src/main/java/org/example/DPK01_impl_1.java
@@ -2,6 +2,10 @@ package org.example;
 
 public class DPK01_impl_1 {
     public static void main(String[] args) {
+        /**
+         * Used StringBuilder class to manipulate a string value, and reverse
+         * option to invert the variable below.
+         **/
         String invertedWord = new StringBuilder("Hello").reverse().toString();
         System.out.println(invertedWord);
     }
