d63e9cb8ed97933cdefd3545a5e1750f5ea5892c
Marcelo Beck <marcelobck@gmail.com>
2025-10-22 19:12:51 -0300

    DPK01_IMPL_04

---
+++ src/main/java/org/example/DPK01_impl_4.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class DPK01_impl_4 {
+    public static String revert(String s) {
+        if (s == null) return null;
+        int n = s.length();
+        char[] stack = new char[n];
+        int top = -1;
+        for (int i = 0; i < n; i++) {
+            stack[++top] = s.charAt(i);
+        }
+        String result = "";
+        while (top >= 0) {
+            result += stack[top--];
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(revert("Hello"));
+    }
+}
