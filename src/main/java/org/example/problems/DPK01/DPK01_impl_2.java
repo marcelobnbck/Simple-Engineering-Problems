d7ff65922aba3dae5cf373101c45e26a7efa9d5a
Marcelo Beck <marcelobck@gmail.com>
2025-10-28 15:22:37 -0300

    New DPK01 problems

---
+++ src/main/java/org/example/problems/DPK01/DPK01_impl_2.java
@@ -0,0 +1,14 @@
+package org.example.problems.DPK01;
+
+public class DPK01_impl_2 {
+    public static void main(String[] args) {
+        String originalWord = "Hello";
+        String invertedWord = "";
+        int size = originalWord.length();
+        while (size > 0) {
+            invertedWord = invertedWord + originalWord.charAt(size - 1);
+            size = size - 1;
+        }
+        System.out.println(invertedWord);
+    }
+}
