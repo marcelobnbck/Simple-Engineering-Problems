b87aa1940bf0b5910779f790821aa33de33e518a
Marcelo Beck <marcelobck@gmail.com>
2025-11-06 10:52:46 -0300

    adding problems to DPK01

---
+++ src/main/java/org/example/problems/DPK01/DPK01_impl_5.java
@@ -0,0 +1,23 @@
+package org.example.problems.DPK01;
+
+import java.util.Stack;
+
+public class DPK01_impl_5 {
+
+    public static String revertWithStack(String str) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : str.toCharArray()) {
+            stack.push(c);
+        }
+        StringBuilder revertedString = new StringBuilder();
+        while (!stack.isEmpty()) {
+            revertedString.append(stack.pop());
+        }
+        return revertedString.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(revertWithStack("Hello"));
+    }
+
+}
---
+++ src/main/java/org/example/problems/DPK01/DPK01_impl_6.java
@@ -0,0 +1,17 @@
+package org.example.problems.DPK01;
+
+public class DPK01_impl_6 {
+    public static String reverseRecursive(String str) {
+        if (str.isEmpty()) {
+            System.out.println("Empty");
+            return str;
+        } else {
+            return reverseRecursive(str.substring(1)) + str.charAt(0);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverseRecursive("Hello"));
+    }
+
+}
