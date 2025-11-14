2cdcfa8b99861e8b35422bd21a46b0966b8ce9a6
Marcelo Beck <marcelobck@gmail.com>
2025-11-13 23:47:01 -0300

    updating dpk02 impl 3

---
+++ src/main/java/org/example/problems/DPK02/DPK02_impl_3.java
@@ -0,0 +1,10 @@
+package org.example.problems.DPK02;
+
+public class DPK02_impl_3 {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5};
+        for (int i = array.length-1; i>=0; i--) {
+            System.out.println(array[i]);
+        }
+    }
+}
