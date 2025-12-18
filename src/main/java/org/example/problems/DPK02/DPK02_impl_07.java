c342de354d136d64d73c3814946f8bf07d580315
Marcelo Beck <marcelobck@gmail.com>
2025-12-18 16:39:48 -0300

    All 2nd exercises refactored

--- src/main/java/org/example/problems/DPK02/DPK02_impl_07.java
+++ src/main/java/org/example/problems/DPK02/DPK02_impl_07.java
@@ -3,13 +3,17 @@ package org.example.problems.DPK02;
 import java.util.ArrayList;
 
 public class DPK02_impl_07 {
+    public static ArrayList<Integer> reverse(int[] input) {
+        ArrayList<Integer> newArray = new ArrayList<>();
+        for (int i = input.length-1; i >= 0; i --) {
+            newArray.add(input[i]);
+        }
+        return newArray;
+    }
+
     public static void main(String[] args) {
         int[] array = {1, 2, 3, 4, 5};
-        ArrayList<Integer> newArray = new ArrayList<>();
-        for (int i = array.length-1; i>=0; i--) {
-            int result = array[i];
-            newArray.add(result);
-        }
-        System.out.println(newArray);
+        ArrayList<Integer> reverseList = reverse(array);
+        System.out.println(reverseList);
     }
 }
--- src/main/java/org/example/problems/DPK02/DPK02_impl_10.java
+++ src/main/java/org/example/problems/DPK02/DPK02_impl_10.java
@@ -3,14 +3,18 @@ package org.example.problems.DPK02;
 import java.util.ArrayList;
 
 public class DPK02_impl_10 {
-    public static void main(String[] args) {
-        int[] array = {1, 2, 3, 4, 5};
+    public static ArrayList<Integer> reverseArray(int[] input) {
         ArrayList<Integer> newArray = new ArrayList<>();
-        for (int i = array.length-1; i>=0; i--) {
-            int result = array[i];
-            newArray.add(result);
+        for (int i = input.length-1; i>=0; i--) {
+            newArray.add(input[i]);
         }
-        System.out.println(newArray);
+        return newArray;
+    }
+
+    public static void main(String[] args) {
+        int[] ascArray = {1, 2, 3, 4, 5};
+        ArrayList<Integer> descArray = reverseArray(ascArray);
+        System.out.println(descArray);
     }
 }
 
