b6fce20ae0a1cca0634236069062620cc31ee039
Marcelo Beck <marcelobck@gmail.com>
2026-02-17 23:49:15 -0300

    adding prb 06

---
+++ src/main/java/org/example/problems/DPK05/DPK05_impl_06.java
@@ -0,0 +1,51 @@
+package org.example.problems.DPK05;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiFunction;
+
+public class DPK05_impl_06 {
+    private static Map<String, Integer> power = Map.of(
+            "John", 100,
+            "Paul", 90,
+            "George", 80,
+            "Ringo", 70
+    );
+
+    private static Map<String, Integer> leaderboard = new HashMap<>(Map.of(
+            "John", 0,
+            "Paul", 0,
+            "George", 0,
+            "Ringo", 0
+    ));
+
+    public static int getPower(String name) {
+        return power.getOrDefault(name, 0);
+    }
+
+    public static String getWinner(String n1, String n2) {
+        int diff = getPower(n1) - getPower(n2);
+        return (diff == 0) ? "Draw" : (diff > 0 ? n1 : n2);
+    }
+
+    public static String play(String n1, String n2) {
+        String winner = getWinner(n1, n2);
+        boolean isDraw = winner.equals("Draw");
+
+        BiFunction<String, Integer, Integer> scoreCalc = (
+                name,
+                current) ->
+                current + (isDraw ? 5 : (name.equals(winner) ? 10 : -5)
+        );
+
+        leaderboard.compute(n1, (k, v) -> scoreCalc.apply(k, v));
+        leaderboard.compute(n2, (k, v) -> scoreCalc.apply(k, v));
+        return winner;
+    }
+
+    public static void main(String[] args) {
+        play("John", "Paul");
+        play("John", "Ringo");
+        System.out.println("Leaderboard: " + leaderboard);
+    }
+}
