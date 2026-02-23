6b6f9fa38c8410960c40aeebe10505f2d8809d56
Marcelo Beck <marcelobck@gmail.com>
2026-02-23 12:36:41 -0300

    Adding 2nd example to show decimal operation

---
+++ src/main/java/org/example/problems/DPK04/DPK04_impl_04_II.java
@@ -0,0 +1,35 @@
+package org.example.problems.DPK04;
+
+public class DPK04_impl_04_II {
+
+    public static String pattern_matcher(String country) {
+        String[] languages = new String[16];
+
+        languages[2] = "Portuguese";
+        languages[3] = "Spanish";
+        languages[5] = "English";
+        languages[6] = "French";
+        languages[7] = "German";
+        languages[9] = "Italian";
+
+        try {
+            int maskedIndex = country.charAt(0) & 0x0F;
+            /**
+             * Can use number 15 instead of 0x0F, as both represent the same value
+             * (15 in decimal and 0x0F in hexadecimal). But using 0x0F it's clearer
+             * to indicate that we are performing a bitwise operation.
+             */
+            //int maskedIndex = country.charAt(0) & 15;
+            String result = languages[maskedIndex];
+            return java.util.Objects.requireNonNullElse(result, "Unknown");
+        } catch (Exception e) {
+            return "Invalid Input";
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(pattern_matcher("Usa"));
+        System.out.println(pattern_matcher("Brazil"));
+        System.out.println(pattern_matcher("Japan"));
+    }
+}
