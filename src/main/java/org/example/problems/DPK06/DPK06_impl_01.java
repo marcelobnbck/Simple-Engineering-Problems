7235a054ab51d1c2ced33b4837202dbabdb0e1d8
Marcelo Beck <marcelobck@gmail.com>
2026-03-04 23:00:55 -0300

    Ex01 adjusts

--- src/main/java/org/example/problems/DPK06/DPK06_impl_01.java
+++ src/main/java/org/example/problems/DPK06/DPK06_impl_01.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class DPK06_impl_01 {
     public static String[] tokenizer(String input, String delimiter) {
-        if (input == null || input.isEmpty()) {
+        if (input == null || delimiter == null) {
             return new String[0];
         }
 
@@ -43,6 +43,12 @@ public class DPK06_impl_01 {
         ));
         System.out.println(Arrays.toString(
                 tokenizer("Hello-World-How-Are-You", "-")
+        ));
+        System.out.println(Arrays.toString(
+                tokenizer("Hello.World.How.Are.You", null)
+        ));
+        System.out.println(Arrays.toString(
+                tokenizer(null, ".")
         ));
     }
 }
