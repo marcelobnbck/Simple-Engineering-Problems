e5b8f08aa9b19518b55257d823d5af48b84be491
Marcelo Beck <marcelobck@gmail.com>
2026-03-12 23:57:43 -0300

    Readme update

--- src/main/java/org/example/problems/DPK06/DPK06_impl_10.java
+++ src/main/java/org/example/problems/DPK06/DPK06_impl_10.java
@@ -1,4 +1,35 @@
 package org.example.problems.DPK06;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DPK06_impl_10 {
+    public static List<String> tokenizer(String text, String token) {
+        List<String> tokens = new ArrayList<>();
+        StringBuilder buffer = new StringBuilder();
+        char delimiter = token.charAt(0);
+
+        for (char c : text.toCharArray()) {
+            switch (c) {
+                case ',':
+                case ' ':
+                case '-':
+                    if (c == delimiter) {
+                        tokens.add(buffer.toString());
+                        buffer.setLength(0);
+                        break;
+                    }
+                default:
+                    buffer.append(c);
+            }
+        }
+        tokens.add(buffer.toString());
+        return tokens;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(tokenizer("Hello,World,How,Are,You", ","));
+        System.out.println(tokenizer("Hello World How Are You", " "));
+        System.out.println(tokenizer("Hello-World-How-Are-You", "-"));
+    }
 }
