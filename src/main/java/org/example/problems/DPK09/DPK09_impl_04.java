ff4f84948edede1a186fa4955672fbe95242cfd5
Marcelo Beck <marcelobck@gmail.com>
2026-04-19 22:49:26 -0300

    Adding 3 and 4 examples to DPK09

--- src/main/java/org/example/problems/DPK09/DPK09_impl_04.java
+++ src/main/java/org/example/problems/DPK09/DPK09_impl_04.java
@@ -3,22 +3,20 @@ package org.example.problems.DPK09;
 import java.util.ArrayList;
 import java.util.List;
 
-public class DPK09_impl_02 {
-    public static List<Integer> whileFilter(int[] input, int number) {
+public class DPK09_impl_04 {
+    public static List<Integer> forFilter(int[] input, int number) {
         List<Integer> result = new ArrayList<>();
-        int index = 0;
-        while (index < input.length) {
-            if (input[index] % number == 0) {
-                result.add(input[index]);
+        for(int i = 0; i < input.length; i++){
+            if(i % number == 0){
+                result.add(i);
             }
-            index++;
         }
         return result;
     }
 
     public static void main(String[] args) {
         int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        List<Integer> result = whileFilter(array, 2);
+        List<Integer> result = forFilter(array, 2);
         System.out.println(result);
     }
 }
