92b48576190fa8ff0a55868804bd174e199c4007
Marcelo Beck <marcelobck@gmail.com>
2026-04-20 23:20:01 -0300

    Adding dpk09 exercises 5 and 6

--- src/main/java/org/example/problems/DPK09/DPK09_impl_06.java
+++ src/main/java/org/example/problems/DPK09/DPK09_impl_06.java
@@ -1,13 +1,13 @@
-package org.example.problems.DPK08;
+package org.example.problems.DPK09;
 
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-public class DPK08_impl_05 {
-    public static List<Integer> iteratorMultiplier(int[] input, int number) {
+public class DPK09_impl_06 {
+    public static List<Integer> iteratorFilter(int[] input, int number) {
         List<Integer> tmpList = new ArrayList<>();
-        for (int i : input) {
+        for(int i : input){
             tmpList.add(i);
         }
 
@@ -16,14 +16,16 @@ public class DPK08_impl_05 {
 
         while (iterator.hasNext()) {
             Integer value = iterator.next();
-            result.add(value * number);
+            if (value % number == 0) {
+                result.add(value);
+            }
         }
         return result;
     }
 
     public static void main(String[] args) {
-        int[] array = {1, 2, 3, 4, 5};
-        List<Integer> result = iteratorMultiplier(array, 2);
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        List<Integer> result = iteratorFilter(array, 2);
         System.out.println(result);
     }
 }
