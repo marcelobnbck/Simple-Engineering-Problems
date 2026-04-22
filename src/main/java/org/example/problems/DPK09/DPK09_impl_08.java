87a3ab5aa7c207625f1344856ea8339c8068a27c
Marcelo Beck <marcelobck@gmail.com>
2026-04-21 23:34:09 -0300

    Finishing DPK09

---
+++ src/main/java/org/example/problems/DPK09/DPK09_impl_08.java
@@ -0,0 +1,24 @@
+package org.example.problems.DPK09;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DPK09_impl_08 {
+    public static List<Integer> filter8(int[] input, int number) {
+        List<Integer> result = new ArrayList<>();
+        int i = 0;
+        while (i < input.length) {
+            if (input[i] % number == 0) {
+                result.add(input[i]);
+            }
+            i++;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        List<Integer> result = filter8(array, 2);
+        System.out.println(result);
+    }
+}
