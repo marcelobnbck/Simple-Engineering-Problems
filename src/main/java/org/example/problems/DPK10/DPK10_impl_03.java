b1c67773bc2550ae3d666c0a7e6a6220e3ef02d2
Marcelo Beck <marcelobck@gmail.com>
2026-04-27 23:44:19 -0300

    Updating problems

--- src/main/java/org/example/problems/DPK10/DPK10_impl_03.java
+++ src/main/java/org/example/problems/DPK10/DPK10_impl_03.java
@@ -1,7 +1,7 @@
 package org.example.problems.DPK10;
 
 public class DPK10_impl_03 {
-    public static Integer reduce(int[] input) {
+    public static Integer reduceWhile(int[] input) {
         int acc = 0;
         int i = 0;
         while (i < input.length) {
@@ -13,7 +13,7 @@ public class DPK10_impl_03 {
 
     public static void main(String[] args) {
         int[] array = {1, 2, 3, 4, 5};
-        Integer result = reduce(array);
+        Integer result = reduceWhile(array);
         System.out.println(result);
     }
 }
