b09b52cf21d6ab95c8d108d8f5bad7a006c4adc1
Marcelo Beck <marcelobck@gmail.com>
2026-04-29 23:27:07 -0300

    Adding 5 and 6 examples

--- src/main/java/org/example/problems/DPK10/DPK10_impl_06.java
+++ src/main/java/org/example/problems/DPK10/DPK10_impl_06.java
@@ -2,15 +2,15 @@ package org.example.problems.DPK10;
 
 import java.util.Arrays;
 
-public class DPK10_impl_05 {
-    public static Integer reduceStream1(int[] input) {
+public class DPK10_impl_06 {
+    public static Integer reduceStream2(int[] input) {
         return Arrays.stream(input)
-                .reduce(0, Integer::sum);
+                .reduce(0, (acc, x) -> acc + x);
     }
 
     public static void main(String[] args) {
         int[] array = {1, 2, 3, 4, 5};
-        Integer result = reduceStream1(array);
+        Integer result = reduceStream2(array);
         System.out.println(result);
     }
 }
