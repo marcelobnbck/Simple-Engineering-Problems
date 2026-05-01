fbd412ab4f7eadd5f8ee9a9ee66241be385afc59
Marcelo Beck <marcelobck@gmail.com>
2026-05-01 11:21:21 -0300

    Finishing DPK10

--- src/main/java/org/example/problems/DPK10/DPK10_impl_08.java
+++ src/main/java/org/example/problems/DPK10/DPK10_impl_08.java
@@ -1,17 +1,18 @@
 package org.example.problems.DPK10;
 
-public class DPK10_impl_07 {
-    public static Integer reduceFor2nd(int[] input) {
+public class DPK10_impl_08 {
+    public static Integer reduceDoWhile2nd(int[] input) {
         int acc = 0;
-        for(int i = 0; i < input.length; i++){
+        int i = 0;
+        do {
             acc += input[i];
-        }
+        } while (++i < input.length);
         return acc;
     }
 
     public static void main(String[] args) {
         int[] array = {1, 2, 3, 4, 5};
-        Integer result = reduceFor2nd(array);
+        Integer result = reduceDoWhile2nd(array);
         System.out.println(result);
     }
 }
