39ded044a3f8c6c5054e86089efb86932bad90a1
Marcelo Beck <marcelobck@gmail.com>
2026-05-22 15:47:48 -0300

    update repo

--- src/main/java/org/example/problems/DPK12/DPK12_impl_01.java
+++ src/main/java/org/example/problems/DPK12/DPK12_impl_01.java
@@ -3,22 +3,32 @@ package org.example.problems.DPK12;
 import java.util.Arrays;
 
 public class DPK12_impl_01 {
-    public static int[] bubbleSort(int[] input) {
-        int[] sorted = input.clone();
-        for (int i = 0; i < sorted.length; i++) {
-            for (int j = 0; j < sorted.length; j++) {
-                if (sorted[i] < sorted[j]) {
-                    int temp = sorted[i];
-                    sorted[i] = sorted[j];
-                    sorted[j] = temp;
+    public static int[] bubbleSortSwap(int[] input) {
+        int[] sorted = new int[input.length];
+
+        for (int i = 0; i < input.length; i++) {
+            sorted[i] = input[i];
+        }
+
+        for (int i = 0; i < sorted.length - 1; i++) {
+            boolean swapped = false;
+            for (int j = 0; j < sorted.length - 1 - i; j++) {
+                if (sorted[j] > sorted[j + 1]) {
+                    int temp = sorted[j];
+                    sorted[j] = sorted[j + 1];
+                    sorted[j + 1] = temp;
+                    swapped = true;
                 }
+            }
+            if (!swapped) {
+                break;
             }
         }
         return sorted;
     }
 
     public static void main(String[] args) {
-        int[] array = {1, 2, 3, 4, 5};
-        System.out.println(Arrays.toString(bubbleSort(array)));
+        int[] array = {5, 4, 3, 2, 1};
+        System.out.println(Arrays.toString(bubbleSortSwap(array)));
     }
 }
