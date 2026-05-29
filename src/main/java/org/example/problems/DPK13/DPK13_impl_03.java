30c5342a74a489283653d759eec624915201c3f8
Marcelo Beck <marcelobck@gmail.com>
2026-05-29 15:46:30 -0300

    dp13 example 3

---
+++ src/main/java/org/example/problems/DPK13/DPK13_impl_03.java
@@ -0,0 +1,28 @@
+package org.example.problems.DPK13;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DPK13_impl_03 {
+    public static List<String> fizzbuzz(int limit) {
+        List<String> result = new ArrayList<>();
+        int i = 1;
+        do {
+            if (i % 3 == 0 && i % 5 == 0) {
+                result.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                result.add("Fizz");
+            } else if (i % 5 == 0) {
+                result.add("Buzz");
+            } else {
+                result.add(String.valueOf(i));
+            }
+            i++;
+        } while (i <= limit);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fizzbuzz(100));
+    }
+}
