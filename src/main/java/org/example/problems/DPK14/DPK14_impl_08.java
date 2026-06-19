346acc9b1df6126e63158261e4852f76d54ac6e5
Marcelo Beck <marcelobck@gmail.com>
2026-06-19 13:23:55 -0300

    Adding problem 08

--- src/main/java/org/example/problems/DPK14/DPK14_impl_08.java
+++ src/main/java/org/example/problems/DPK14/DPK14_impl_08.java
@@ -1,55 +1,65 @@
 package org.example.problems.DPK14;
 
-import java.util.List;
+import java.util.Arrays;
 
 public class DPK14_impl_08 {
-    public static String[] fighterWalk(String[][] grid, int row, int column, int[] moves) {
-        String fighter = grid[row][column];
-        String[] beaten = new String[20];
-        int count = 0;
+        public static String[] fighterWalk(String[][] grid, int startRow, int startColumn, String[] moves) {
+            int row = startRow;
+            int column = startColumn;
+            String fighter = grid[row][column];
+            String[] beaten = new String[20];
+            int beatenIndex = 0;
 
-        for(int i = 0; i < moves.length; i++){
-            if(moves[i] == 0){
-                row--;
+            for (int i = 0; i < moves.length; i++) {
+                int newRow = row;
+                int newColumn = column;
+
+                if (moves[i] == "up") {
+                    newRow = row - 1;
+                    if (newRow < 0) {
+                        newRow = grid.length - 1;
+                    }
+                }
+                if (moves[i] == "down") {
+                    newRow = row + 1;
+                    if (newRow >= grid.length) {
+                        newRow = 0;
+                    }
+                }
+                if (moves[i] == "left") {
+                    newColumn = column - 1;
+                    if (newColumn < 0) {
+                        newColumn = grid[0].length - 1;
+                    }
+                }
+                if (moves[i] == "right") {
+                    newColumn = column + 1;
+                    if (newColumn >= grid[0].length) {
+                        newColumn = 0;
+                    }
+                }
+
+                if (grid[newRow][newColumn] != "") {
+                    beaten[beatenIndex] = grid[newRow][newColumn];
+                    beatenIndex++;
+                }
+                grid[row][column] = "";
+
+                grid[newRow][newColumn] = fighter;
+
+                row = newRow;
+                column = newColumn;
             }
-            if(moves[i] == 1){
-                row++;
-            }
-            if(moves[i] == 2){
-                column--;
-            }
-            if(moves[i] == 3){
-                column++;
-            }
-            if(row < 0){
-                row = grid.length - 1;
-            }
-            if(row >= grid.length){
-                row = 0;
-            }
-            if(column < 0){
-                column = grid[0].length - 1;
-            }
-            if(column >= grid[0].length){
-                column = 0;
-            }
-            if(grid[row][column] != ""){
-                beaten[count] = grid[row][column];
-                count++;
-            }
-            grid[row][column] = fighter;
+            return beaten;
         }
-        return beaten;
-    }
 
     public static void main(String[] args) {
         String[][] grid = {
                 {"Ryu", "E.Honda", "Blanka", "Guile", "Balrog", "Vega"},
                 {"Ken", "Chun Li", "Zangief", "Dhalsim", "Sagat", "M.Bison"}
         };
-        int[] moves = {0,2,1,3};
-        String[] directions = {"up", "left", "down", "right"};
-        List<String> beaten = List.of(fighterWalk(grid, 0, 0, moves));
-        System.out.println(beaten);
+        String[] moves = {"up", "left", "down", "right"};
+        String[] beaten = fighterWalk(grid, 0, 0, moves);
+        System.out.println(Arrays.toString(beaten));
     }
 }
