466abc75f0a36b2a00a071323cb6e8460def3f08
Marcelo Beck <marcelobck@gmail.com>
2026-06-24 00:19:15 -0300

    Fix problem01 from dpk15

--- src/main/java/org/example/problems/DPK15/DPK15_impl_01.java
+++ src/main/java/org/example/problems/DPK15/DPK15_impl_01.java
@@ -3,24 +3,49 @@ package org.example.problems.DPK15;
 import java.util.ArrayList;
 
 public class DPK15_impl_01 {
-    class Person {
-        String name;
-        int age;
-        ArrayList<String> friend = new ArrayList<>();
-        Person(String name, int age, ArrayList<String> friend) {
-            this.name = name;
-            this.age = age;
-            this.friend = friend;
+    public static class Person {
+        private String name;
+        private int age;
+        private ArrayList<String> friends;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String newName) {
+            this.name = newName;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int newAge) {
+            this.age = newAge;
+        }
+
+        public ArrayList<String> getFriends() {
+            return friends;
+        }
+
+        public void setFriends(ArrayList<String> newFriends) {
+            this.friends = newFriends;
         }
     }
 
     public static void main(String[] args) {
-        ArrayList<String> friends = new ArrayList<>();
-        friends.add("Alice");
-        friends.add("Bob");
-        Person person = new DPK15_impl_01().new Person("John", 30, friends);
-        System.out.println("Name: " + person.name);
-        System.out.println("Age: " + person.age);
-        System.out.println("Friends: " + person.friend);
+        Person p1 = new Person();
+
+        p1.setName("John");
+        p1.setAge(30);
+        p1.setFriends(new ArrayList<>() {{
+            add("Paul");
+            add("George");
+            add("Ringo");
+        }});
+
+        System.out.println("Friends: " + p1.getFriends());
+        System.out.println("Age: " + p1.getAge());
+        System.out.println("Name: " + p1.getName());
     }
 }
